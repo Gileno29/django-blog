43ca05179c628b8ab3651f2ce08b5a5dc3616f96
Gileno <gileno.cr.duarte@gmail.com>
2024-08-02 08:41:46 -0300

    feat: Database trocado para postgres

--- core/settings.py
+++ core/settings.py
@@ -77,9 +77,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR , 'db.sqlite3'),
-    }
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": 'dbblog',
+        "USER": 'userblog',
+        "PASSWORD": 'blog',
+        "HOST": 'localhost',
+        "PORT":'5432',
+    },
+  
 }
 
 
